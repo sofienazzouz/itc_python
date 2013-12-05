e451e2d66348af770d6e4bf6d0ca271090160807
sofien azzouz <sofienazzouz@yahoo.fr>
2013-12-05 17:27:57 +0100

    Update itc.sh

--- itc.sh
+++ itc.sh
@@ -6,17 +6,17 @@ APP_SKU="$4"
 APP_NAME=$APP_SKU'.itmsp'
 APP_VERSION=$5
 
-if [ "$1" = "fetch" ]; then
+if [ "$1" = "fetch" ]; then # download package that contain metadata.xml
     /Applications/Xcode.app/Contents/Applications/Application\Loader.app/Contents/MacOS/itms/bin/iTMSTransporter -m lookupMetadata -u $USERNAME -p $PASSWORD -vendor_id $APP_SKU -destination .
     if [ "$?" = "0" ]; then
         echo "Success! Metadata downloaded."
     fi
 else
-    if [ "$1" = "update" ]; then
+    if [ "$1" = "update" ]; then #execute update script the verifie the new metadata.xml
         # update screeshots
         python screenshots_updater.py $APP_SKU $APP_VERSION
         /Applications/Xcode.app/Contents/Applications/Application\Loader.app/Contents/MacOS/itms/bin/iTMSTransporter -m verify -f $APP_NAME  -u $USERNAME -p $PASSWORD
-        if [ "$?" = "0" ]; then
+        if [ "$?" = "0" ]; then #if verification success then upload the new metadata.xml
             /Applications/Xcode.app/Contents/Applications/Application\Loader.app/Contents/MacOS/itms/bin/iTMSTransporter -m upload -f $APP_NAME -u $USERNAME -p $PASSWORD
         else
             echo "ERROR: Invalid metadata!"
